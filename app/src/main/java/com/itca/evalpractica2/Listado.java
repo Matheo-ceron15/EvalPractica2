eb63df23f338dea8503755d88cb1cbcdded6b64d
Matheo-ceron15 <mateovasques6@gmail.com>
2021-09-23 10:09:54 -0600

    Metodo guardar terminado

---
+++ app/src/main/java/com/itca/evalpractica2/Listado.java
@@ -0,0 +1,22 @@
+package com.itca.evalpractica2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Listado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado);
+
+    }
+    public void regresar(View view) {
+        Intent intent = new Intent(Listado.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/itca/evalpractica2/VistaNota.java
@@ -0,0 +1,16 @@
+package com.itca.evalpractica2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VistaNota extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_nota);
+
+
+    }
+}
